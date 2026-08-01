2ada09e2cb200e6def265d90c3a81618264be3bb
asadtraders <asadtrader.in@gmail.com>
2026-08-01 18:10:41 +0500

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,12 @@
+module.exports = {
+    startimage: 'https://files.catbox.moe/bjzc47.png',
+    ownerNumber: process.env.OWNER_NUMBER || '923267613661',
+    botName: 'ASAD MINI BOT',
+    ownerName: 'ASAD KHAN',
+    whatsappChannel: 'https://whatsapp.com/channel/0029VbDWQ5ZLtOj7pwM8ge3O',
+    tgOwnerId: process.env.OWNER_TELEGRAM_ID || '8488081516',
+    premiumUsers: [],
+    connectedBots: [],
+    version: '3.0.0',
+    prefix: '.'
+};
